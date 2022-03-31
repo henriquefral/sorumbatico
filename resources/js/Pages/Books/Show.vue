07dd9c2a1ce4f1a9c9fa78e1201d0faabcae3605
gessilt <henriquefreitas2710@gmail.com>
2022-03-31 09:45:24 -0300

    refactor

--- resources/js/Pages/Books/Show.vue
+++ resources/js/Pages/Books/Show.vue
@@ -6,7 +6,7 @@
                     Books
                 </h2>
                 
-                <jet-button class="" @click.prevent="isModalActive = !isModalActive">
+                <jet-button class="" @click.prevent="isModalAddActive = !isModalAddActive">
                     Add new book
                 </jet-button>
             </div>
@@ -19,11 +19,11 @@
                 <template>
                     <jet-section-border />
 
-                    <add-new-book class="mt-10 sm:mt-0"  @closeModal="closeModal" 
-                    @submit="submit" :isModalActive="isModalActive" :processing="form.processing" 
+                    <modal-add-new class="mt-10 sm:mt-0"  @closeModal="closeModal" 
+                    @store="store" :isModalActive="isModalAddActive" :processing="form.processing" 
                     title="Add new book"
                     >
-                        <form @submit.prevent="submit">
+                        <form @submit.prevent="store">
                             <div class="mt-4">
                                 <jet-label class="mt-1 text-lg" value="Book's name"/> 
                                 <jet-input type="text" class="mt-1 block w-3/4" placeholder="Name"
@@ -67,13 +67,62 @@
                                 <jet-validation-errors class="mt-4"/>
                             </div>
                         </form>
-                    </add-new-book>
+                    </modal-add-new>
+                    <modal-add-new class="mt-10 sm:mt-0"  @closeModal="closeModal" 
+                    @store="store" :isModalActive="isModalAddActive" :processing="form.processing" 
+                    title="Add new book"
+                    >
+                        <form @submit.prevent="store">
+                            <div class="mt-4">
+                                <jet-label class="mt-1 text-lg" value="Book's name"/> 
+                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Name"
+                                    ref="name"
+                                    v-model="form.name" 
+                                />
+                                <jet-label class="mt-1 text-lg" value="Book's author"/> 
+                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Author"
+                                    ref="author"
+                                    v-model="form.author" 
+                                />
+                                <jet-label class="mt-1 text-lg" value="Total number of pages"/> 
+                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Total pages"
+                                    ref="pages"
+                                    v-model="form.pages" 
+                                />
+                                <jet-label class="mt-1 text-lg" value="Number of readed pages"/> 
+                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Pages readed"
+                                    ref="current_page"
+                                    v-model="form.current_page" 
+                                />
+                                <div class="flex items-center space-x-10 justify"> 
+                                    <div class="flex items-center">
+                                        <jet-label class="mt-1 text-lg mr-1" 
+                                        value="Is an favorite?"/> 
+                                        <jet-checkbox class="mt-1 block" 
+                                            ref="favorite"
+                                            v-model="form.favorite" 
+                                        />
+                                    </div>
+                                    <div class="flex items-center ">
+                                        <jet-label class="mt-1 text-lg mr-1" 
+                                        value="Is for leisure?"/> 
+                                        <jet-checkbox class="mt-1 block"
+                                            :checked="true"
+                                            ref="leisure"
+                                            v-model="form.leisure" 
+                                        />
+                                    </div>
+                                </div>  
+                                <jet-validation-errors class="mt-4"/>
+                            </div>
+                        </form>
+                    </modal-add-new>
                 </template>
             </div>
         </div>
-        
-        <render-book :books="books"/>
-        
+        <div class="flex justify-center flex-wrap mx-44 space-x-2 ">
+            <render-item :itens="books" :itemName="'books'"/>
+        </div>
         
     </app-layout>
 </template>
@@ -87,8 +136,8 @@
     import JetCheckbox from '@/Jetstream/Checkbox.vue';
     import JetLabel from '@/Jetstream/Label.vue';
     import JetValidationErrors from '@/Jetstream/ValidationErrors.vue';
-    import AddNewBook from '../../Components/ModalAddNew.vue';
-    import RenderBook from '../../Components/RenderItem.vue';
+    import ModalAddNew from '../../Components/ModalAddNew.vue';
+    import RenderItem from '../../Components/RenderItem.vue';
 
 
     export default defineComponent({
@@ -103,15 +152,15 @@
             JetCheckbox,
             JetLabel,
             JetValidationErrors,
-            AddNewBook,
-            RenderBook
+            ModalAddNew,
+            RenderItem
         },
         created(){
             console.log(this.books)
         },
         data(){
             return{
-                isModalActive: false,
+                isModalAddActive: false,
                 form: this.$inertia.form({
                     name: '',
                     author:'',
@@ -124,16 +173,13 @@
         },
         methods:{
             closeModal(){
-                this.isModalActive = false;
+                this.isModalAddActive = false;
             },
-            submit() {
+            store() {
                 this.form.post(this.route('books.store'), {
-                    onFinish: () => {
-                        if(this.$page.props.errors){
-                            
-                        } else {
-                            this.closeModal()
-                        }
+                    onSuccess: () => {
+                        this.form.reset();
+                        this.closeModal();
                     }
                 })
             },
