a0ec333fd65c29389ad52b20fba95ab26ce379a2
gessilt <henriquefreitas2710@gmail.com>
2022-03-31 09:45:11 -0300

    feat: dynamic and destroy method

--- resources/js/Components/RenderItem.vue
+++ resources/js/Components/RenderItem.vue
@@ -1,22 +1,39 @@
 <template>
-    <div class="flex justify-center flex-wrap mx-44 space-x-2 ">
-        <div class="p-6 bg-white border-b border-gray-200 mb-2.5 m-2/6 max-w-[25%]" v-for="(book) in books" :key="book.id">                
-            <div class="mt-8 text-2xl">
-                {{ book.name }}
-            </div>
-
-            <div class="mt-6 text-gray-500">
-                {{book.pages}}/{{book.current_page}}
+    <div class="p-6 bg-white border-b border-gray-200 mb-2.5 m-2/6 max-w-[25%]" v-for="(item) in itens" :key="item.id">                
+        <div class="mt-8 text-2xl">
+            {{ item.name }}
+        </div>
+        <div class="mt-6 text-gray-500">
+            <div class="flex justify-between">
+                <div v-if="itemName=='books'">
+                    {{item.pages}}/{{item.current_page}}
+                </div>
+                <div v-if="itemName=='Mangas'">
+                    {{item.chapters}}/{{item.current_chapter}}
+                </div>
+                <icon name="delete" class="w-5 h-5 fill-current hover:text-red-900 cursor-pointer" 
+                @click="destroy(item)"/>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Icon from './Icons.vue';
+
     export default {
         props:{
-            books: Object
+            itens: Object,
+            itemName: String,
         },
+        components:{
+            Icon
+        },
+        methods:{
+            destroy(item){
+                this.$inertia.delete(`/${this.itemName}/${item.id}`);
+            }
+        }
     }
 </script>
 
