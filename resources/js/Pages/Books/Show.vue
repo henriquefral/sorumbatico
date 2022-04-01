8ec143cc303080ca708ecfcea9e590cf4e9e3284
gessilt <henriquefreitas2710@gmail.com>
2022-04-01 11:21:03 -0300

    feat: CRUD

--- resources/js/Pages/Books/Show.vue
+++ resources/js/Pages/Books/Show.vue
@@ -19,116 +19,153 @@
                 <template>
                     <jet-section-border />
 
-                    <modal-add-new class="mt-10 sm:mt-0"  @closeModal="closeModal" 
+                    <modal-add-new class="mt-10 sm:mt-0"  @closeModal="closeAddModal" 
                     @store="store" :isModalActive="isModalAddActive" :processing="form.processing" 
-                    title="Add new book"
+                    title="Add new book" :method="store"
                     >
-                        <form @submit.prevent="store">
-                            <div class="mt-4">
-                                <jet-label class="mt-1 text-lg" value="Book's name"/> 
-                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Name"
-                                    ref="name"
-                                    v-model="form.name" 
-                                />
-                                <jet-label class="mt-1 text-lg" value="Book's author"/> 
-                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Author"
-                                    ref="author"
-                                    v-model="form.author" 
-                                />
-                                <jet-label class="mt-1 text-lg" value="Total number of pages"/> 
-                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Total pages"
-                                    ref="pages"
-                                    v-model="form.pages" 
-                                />
-                                <jet-label class="mt-1 text-lg" value="Number of readed pages"/> 
-                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Pages readed"
-                                    ref="current_page"
-                                    v-model="form.current_page" 
-                                />
-                                <div class="flex items-center space-x-10 justify"> 
-                                    <div class="flex items-center">
-                                        <jet-label class="mt-1 text-lg mr-1" 
-                                        value="Is an favorite?"/> 
-                                        <jet-checkbox class="mt-1 block" 
-                                            ref="favorite"
-                                            v-model="form.favorite" 
-                                        />
-                                    </div>
-                                    <div class="flex items-center ">
-                                        <jet-label class="mt-1 text-lg mr-1" 
-                                        value="Is for leisure?"/> 
-                                        <jet-checkbox class="mt-1 block"
-                                            :checked="true"
-                                            ref="leisure"
-                                            v-model="form.leisure" 
-                                        />
-                                    </div>
-                                </div>  
-                                <jet-validation-errors class="mt-4"/>
-                            </div>
-                        </form>
+                        <template #content> 
+                            <form @submit.prevent="store">
+                                <div class="mt-4">
+                                    <jet-label class="mt-1 text-lg" value="Book's name"/> 
+                                    <jet-input type="text" class="mt-1 block w-3/4" placeholder="Name"
+                                        ref="name"
+                                        v-model="form.name" 
+                                    />
+                                    <jet-label class="mt-1 text-lg" value="Book's author"/> 
+                                    <jet-input type="text" class="mt-1 block w-3/4" placeholder="Author"
+                                        ref="author"
+                                        v-model="form.author" 
+                                    />
+                                    <jet-label class="mt-1 text-lg" value="Total number of pages"/> 
+                                    <jet-input type="text" class="mt-1 block w-3/4" placeholder="Total pages"
+                                        ref="pages"
+                                        v-model="form.pages" 
+                                    />
+                                    <jet-label class="mt-1 text-lg" value="Number of readed pages"/> 
+                                    <jet-input type="text" class="mt-1 block w-3/4" placeholder="Pages readed"
+                                        ref="current_page"
+                                        v-model="form.current_page" 
+                                    />
+                                    <div class="flex items-center space-x-10 justify"> 
+                                        <div class="flex items-center">
+                                            <jet-label class="mt-1 text-lg mr-1" 
+                                            value="Is an favorite?"/> 
+                                            <jet-checkbox class="mt-1 block" 
+                                                ref="favorite"
+                                                v-model="form.favorite" 
+                                            />
+                                        </div>
+                                        <div class="flex items-center ">
+                                            <jet-label class="mt-1 text-lg mr-1" 
+                                            value="Is for leisure?"/> 
+                                            <jet-checkbox class="mt-1 block"
+                                                :checked="true"
+                                                ref="leisure"
+                                                v-model="form.leisure" 
+                                            />
+                                        </div>
+                                    </div>  
+                                    <jet-validation-errors class="mt-4"/>
+                                </div>
+                            </form>
+                        </template>
                     </modal-add-new>
-                    <modal-add-new class="mt-10 sm:mt-0"  @closeModal="closeModal" 
-                    @store="store" :isModalActive="isModalAddActive" :processing="form.processing" 
-                    title="Add new book"
-                    >
-                        <form @submit.prevent="store">
-                            <div class="mt-4">
-                                <jet-label class="mt-1 text-lg" value="Book's name"/> 
-                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Name"
-                                    ref="name"
-                                    v-model="form.name" 
-                                />
-                                <jet-label class="mt-1 text-lg" value="Book's author"/> 
-                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Author"
-                                    ref="author"
-                                    v-model="form.author" 
-                                />
-                                <jet-label class="mt-1 text-lg" value="Total number of pages"/> 
-                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Total pages"
-                                    ref="pages"
-                                    v-model="form.pages" 
-                                />
-                                <jet-label class="mt-1 text-lg" value="Number of readed pages"/> 
-                                <jet-input type="text" class="mt-1 block w-3/4" placeholder="Pages readed"
-                                    ref="current_page"
-                                    v-model="form.current_page" 
-                                />
-                                <div class="flex items-center space-x-10 justify"> 
-                                    <div class="flex items-center">
-                                        <jet-label class="mt-1 text-lg mr-1" 
-                                        value="Is an favorite?"/> 
-                                        <jet-checkbox class="mt-1 block" 
-                                            ref="favorite"
-                                            v-model="form.favorite" 
-                                        />
+                </template>
+                <template>
+                    <div>
+                        <modal-add-new class="mt-10 sm:mt-0"  @closeModal="closeUpdateModal" 
+                        @update="update" :isModalActive="isModalUpdateActive" :processing="form.processing" 
+                        title="Update new book" :method="update"
+                        >
+                            <template #title>
+                                <icon name="delete" class="w-5 h-5 fill-current hover:text-red-900 cursor-pointer" @click="destroy(book.id)"/>
+                            </template>
+                            <template #content>
+                                <div class="flex flex-row space-x-1">
+                                    <div class="max-w-[35%]">
+                                        <div>
+                                            Book name: {{book.name}}
+                                        </div>
+                                        <div>
+                                            Author: {{book.author}}
+                                        </div>
+                                        <div>
+                                            Progress: {{book.pages}}/{{book.current_page}}
+                                        </div>
+                                        <div>
+                                            Is favorite? {{book.favorite == 1 ? 'Yes' : 'No'}}
+                                            <br/>
+                                            Its for leisure? {{book.leisure == 1 ? 'Yes' : 'No'}}
+
+                                        </div>
                                     </div>
-                                    <div class="flex items-center ">
-                                        <jet-label class="mt-1 text-lg mr-1" 
-                                        value="Is for leisure?"/> 
-                                        <jet-checkbox class="mt-1 block"
-                                            :checked="true"
-                                            ref="leisure"
-                                            v-model="form.leisure" 
-                                        />
-                                    </div>
-                                </div>  
-                                <jet-validation-errors class="mt-4"/>
-                            </div>
-                        </form>
-                    </modal-add-new>
+                                    <div class="divide-x-2 divide-black mt-3"> </div>
+                                    <form @submit.prevent="store">
+                                        <div class="w-full ">
+                                            <jet-label class="text-lg" value="Book's name"/> 
+                                            <jet-input type="text" class="mt-1 block" placeholder="Name"
+                                                ref="name"
+                                                :modelValue="book.name"
+                                                v-model="form.name" 
+                                            />
+                                            <jet-label class="mt-1 text-lg" value="Book's author"/> 
+                                            <jet-input type="text" class="mt-1 block" placeholder="Author"
+                                                ref="author"
+                                                :modelValue="book.author"
+                                                v-model="form.author" 
+                                            />
+                                            <jet-label class="mt-1 text-lg" value="Total number of pages"/> 
+                                            <jet-input type="text" class="mt-1 block" placeholder="Total pages"
+                                                ref="pages"
+                                                :modelValue="book.pages"
+                                                v-model="form.pages" 
+                                            />
+                                            <jet-label class="mt-1 text-lg" value="Number of readed pages"/> 
+                                            <jet-input type="text" class="mt-1 block" placeholder="Pages readed"
+                                                ref="current_page"
+                                                :modelValue="book.current_page"
+                                                v-model="form.current_page" 
+                                            />
+                                            <div class="flex items-center space-x-10 justify"> 
+                                                <div class="flex items-center">
+                                                    <jet-label class="mt-1 text-lg mr-1" 
+                                                    value="Is an favorite?"/> 
+                                                    <jet-checkbox class="mt-1" 
+                                                        :checked="book.favorite == 0 ? false : true"
+                                                        ref="favorite"
+                                                        v-model="form.favorite" 
+                                                    />
+                                                </div>
+                                                <div class="flex items-center ">
+                                                    <jet-label class="mt-1 text-lg mr-1" 
+                                                    value="Is for leisure?"/> 
+                                                    <jet-checkbox class="mt-1"
+                                                        :checked="book.leisure == 0 ? false : true"
+                                                        ref="leisure"
+                                                        v-model="form.leisure" 
+                                                    />
+                                                </div>
+                                            </div>  
+                                            <jet-validation-errors class="mt-4"/>
+                                        </div>
+                                    </form>
+                                </div>
+                            </template>
+                        </modal-add-new>
+                    </div>
                 </template>
             </div>
         </div>
         <div class="flex justify-center flex-wrap mx-44 space-x-2 ">
-            <render-item :itens="books" :itemName="'books'"/>
+            <render-item :itens="books" :itemName="'books'"
+            @updateModal="updateModal"
+            />
         </div>
         
     </app-layout>
 </template>
 
 <script>
-    import { defineComponent } from 'vue';
     import AppLayout from '@/Layouts/AppLayout.vue';
     import JetButton from '@/Jetstream/Button.vue';
     import JetSectionBorder from '@/Jetstream/SectionBorder.vue';
@@ -138,9 +175,10 @@
     import JetValidationErrors from '@/Jetstream/ValidationErrors.vue';
     import ModalAddNew from '../../Components/ModalAddNew.vue';
     import RenderItem from '../../Components/RenderItem.vue';
+    import Icon from '../../Components/Icons.vue';
 
 
-    export default defineComponent({
+    export default {
         props:{
             books: Object
         },
@@ -153,7 +191,8 @@
             JetLabel,
             JetValidationErrors,
             ModalAddNew,
-            RenderItem
+            RenderItem,
+            Icon
         },
         created(){
             console.log(this.books)
@@ -161,6 +200,8 @@
         data(){
             return{
                 isModalAddActive: false,
+                isModalUpdateActive: false,
+                book: {},
                 form: this.$inertia.form({
                     name: '',
                     author:'',
@@ -172,18 +213,40 @@
             }
         },
         methods:{
-            closeModal(){
+            closeAddModal(){
+                this.form.reset();
                 this.isModalAddActive = false;
+            },
+            closeUpdateModal(){
+                this.form.reset();
+                this.isModalUpdateActive = false;
+            },
+            updateModal(childProps){
+                this.book = childProps.book;
+                this.isModalUpdateActive = childProps.value;
             },
             store() {
                 this.form.post(this.route('books.store'), {
                     onSuccess: () => {
-                        this.form.reset();
-                        this.closeModal();
+                        this.closeAddModal();
                     }
-                })
+                });
+            },
+            update() {
+                this.form.patch((`/books/${this.book.id}`), {
+                    onSuccess: () => {
+                        this.closeUpdateModal();
+                    },
+                });
+            },
+            destroy(){
+                this.$inertia.delete((`/books/${this.book.id}`), {
+                    onSuccess: () => {
+                        this.closeUpdateModal();
+                    }
+                });
             },
 
         }
-    })
+    }
 </script>
