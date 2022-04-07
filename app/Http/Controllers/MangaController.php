d9501e860735fee838fe4ab884e5fa91224193a5
gessilt <henriquefreitas2710@gmail.com>
2022-04-07 10:43:50 -0300

    feat: CRUD

--- app/Http/Controllers/MangaController.php
+++ app/Http/Controllers/MangaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Manga;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
 
 class MangaController extends Controller
@@ -15,7 +16,8 @@ class MangaController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Mangas/Show');
+        $data = Manga::all()->where('user_id','=',auth()->user()->id);
+        return Inertia::render('Mangas/Show', ['mangas' => $data]);
     }
 
     /**
@@ -36,7 +38,32 @@ class MangaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:150'],
+            'author' => ['required', 'string', 'max:150'],
+            'chapters' => ['required','int', 'min:0'],
+            'current_chapter' => ['int','nullable', 'min:0'],
+            ],[
+            'name.required'=>'Manga name is required',
+            'name.max'=>'Manga name is too big',
+            'author.required'=>'Manga author is required',
+            'author.max'=>'Manga author is too big',
+            'pages.min' => 'Please, a valid number for the chapter',
+            'pages.required'=>'Chapters are required',
+            'current_page.int' => 'The Chapters must be a numerical value',
+            'current_page.min' => 'Please, a valid number for the chapter',
+            'pages.int' => 'The chapters must be a numerical value',
+        ]);
+        Manga::create([
+            'name' => $request->name,
+            'user_id' => auth()->user()->id,
+            'author' => $request->author,
+            'chapters' => $request->chapters,
+            'current_chapter' => $request->current_chapter,
+            'favorite' => $request->favorite,
+        ]);
+
+        return back();
     }
 
     /**
@@ -68,9 +95,32 @@ class MangaController extends Controller
      * @param  \App\Models\Manga  $manga
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Manga $manga)
+    public function update(Request $request, $id)
     {
-        //
+        Validator::make($request->all(), [
+            'name' => ['nullable', 'string', 'max:150'],
+            'author' => ['nullable', 'string', 'max:150'],
+            'chapters' => ['nullable','int', 'min:0'],
+            'current_chapter' => ['int','nullable', 'min:0'],
+            ],[
+            'name.max'=>'Manga name is too big',
+            'author.max'=>'Manga author is too big',
+            'pages.min' => 'Please, a valid number for the chapter',
+            'current_page.int' => 'The Chapters must be a numerical value',
+            'current_page.min' => 'Please, a valid number for the chapter',
+            'pages.int' => 'The chapters must be a numerical value',
+        ])->validate();
+        $update = [];
+        foreach($request->all() as $field => $value)
+        {
+            if (isset($value)) {
+                $update = array_merge($update,[$field => $value]);
+            }
+        };
+
+        Manga::find($id)->update($update); 
+
+        return back();
     }
 
     /**
@@ -79,8 +129,10 @@ class MangaController extends Controller
      * @param  \App\Models\Manga  $manga
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Manga $manga)
+    public function destroy($id)
     {
-        //
+        Manga::destroy($id);
+        
+        return back();
     }
 }
